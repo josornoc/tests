cbabd456fc5dfe7711d05280e399aeb5c4c94a56
Jose Osorno <josornoc@users.noreply.github.com>
2018-03-21 17:10:14 +0100

    Update app.rb

--- api/app.rb
+++ api/app.rb
@@ -1,14 +1,12 @@
 require 'net/http'
 require 'json'
 
-# You must create a REST api client for the api: https://dog.ceo/dog-api/
-# Your client must be capable of list breeds and return a random breed image
-# of a specified breed
-
+# You must create a RESTful resource that implements the API: https://dog.ceo/dog-api/
+# Your client must be capable of list breeds and return a random breed image of a specified breed
 # You can find information about the two endpoints in the documentation: https://dog.ceo/dog-api/#breed
 
-# Method "all" must return the list of master breeds in array format
+# Method "all" must return the list of master breeds as an array
 puts client.all
 
-# Method must return the random url from a breed in string format
+# Method must return a random image url from a breed
 puts client.random_image_url_by_breed('corgi')
